c6effbb48ec62280738d51f27269ba97d6d82f84
Brian Low <github@brianlow.com>
2022-03-26 23:25:37 -0600

    Remove unused #defines

--- keyboards/sofle/keymaps/choc_via/keymap.c
+++ keyboards/sofle/keymaps/choc_via/keymap.c
@@ -24,17 +24,6 @@ enum sofle_layers {
 
 #include "oled.c"
 
-#define LOWER  MO(_LOWER)
-#define RAISE  MO(_RAISE)
-#define ADJUST MO(_ADJUST)
-
-#define PAUSE      KC_MEDIA_PLAY_PAUSE
-#define DEL_LINE   LCTL(KC_BACKSPACE)
-#define PREV_WORD  LOPT(KC_LEFT)
-#define NEXT_WORD  LOPT(KC_RIGHT)
-#define TAB_LEFT   LSFT(LCMD(KC_LEFT_BRACKET))
-#define TAB_RIGHT  LSFT(LCMD(KC_RIGHT_BRACKET))
-
 enum custom_keycodes {
     KC_LOWER = SAFE_RANGE,
     KC_RAISE,
@@ -65,11 +54,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_QWERTY] = LAYOUT(
-    KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,     KC_7,     KC_8,    KC_9,    KC_0,    KC_GRV,
-    KC_ESC,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,     KC_U,     KC_I,    KC_O,    KC_P,    KC_BSPC,
-    KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,     KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_MUTE,   PAUSE, KC_N,     KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                      KC_LGUI, KC_LALT, KC_LCTRL,KC_LOWER,KC_ENT,    KC_SPC,KC_RAISE, KC_RCTRL, KC_RALT, KC_RGUI
+    KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,     KC_7,     KC_8,    KC_9,    KC_0,    KC_GRV,
+    KC_ESC,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                       KC_Y,     KC_U,     KC_I,    KC_O,    KC_P,    KC_BSPC,
+    KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                       KC_H,     KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_MUTE,   KC_MPLY,KC_N,     KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+                      KC_LGUI, KC_LALT, KC_LCTRL,KC_LOWER,KC_ENT,    KC_SPC, KC_RAISE, KC_RCTRL, KC_RALT, KC_RGUI
 ),
 /* LOWER
  * ,----------------------------------------.                    ,-----------------------------------------.
