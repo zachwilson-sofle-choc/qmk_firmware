33dacf1d8e1d847d8c517ab4680a772b45681dca
Zach Wilson <zakrywilson@gmail.com>
2023-05-21 18:01:06 -0500

    Reducing QMK footprint

--- keyboards/sofle_choc/keymaps/arnjwlsn/rules.mk
+++ keyboards/sofle_choc/keymaps/arnjwlsn/rules.mk
@@ -16,8 +16,12 @@ LTO_ENABLE = yes
 # Enable chording
 COMBO_ENABLE = yes
 
+# Reduce footprint: https://docs.qmk.fm/#/squeezing_avr?id=squeezing-the-most-out-of-avr
 COMMAND_ENABLE = no
 CONSOLE_ENABLE = no
+GRAVE_ESC_ENABLE = no
+MOUSEKEY_ENABLE = no
+MUSIC_ENABLE = no
 SPACE_CADET_ENABLE = no
 VERBOSE = no
 VIA_ENABLE = no
