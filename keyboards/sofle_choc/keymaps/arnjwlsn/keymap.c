3a1d31289d29b8afde25905c1ba6faf1ae4fb467
Aaron Wilson <awilson2994@gmail.com>
2023-05-28 10:35:51 -0600

    Swap nav and sym layer locations

--- keyboards/sofle_choc/keymaps/arnjwlsn/keymap.c
+++ keyboards/sofle_choc/keymaps/arnjwlsn/keymap.c
@@ -164,7 +164,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|  Mute |        |       |------+------+------+------+------+------|
  * | LCtrl|   Z  |   X  |   C  |   V  |   B  |-------'        `-------|   N  |   M  |   ,  |   .  |   /  |  \   |
  * `-----------------------------------------'  /------/    \------\  '-----------------------------------------'
- *               | LAlt | Shft | NAV  | Spc  | / Bspc /      \ Ent  \ | Shft | NAV  | SYM  | LWin |
+ *               | LAlt | Shft | SYM  | Spc  | / Bspc /      \ Ent  \ | Shft | SYM  | NAV  | LWin |
  *               `----------------------------/------/        \------\----------------------------'
  */
   [_BASE] = LAYOUT(
@@ -172,7 +172,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,  KC_Q, KC_W,    KC_E,    KC_R, KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_MINS,
       KC_ESC,  KC_A, KC_S,    KC_D,    KC_F, KC_G,                        KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT,
       KC_LCTL, KC_Z, KC_X,    KC_C,    KC_V, KC_B,   KC_MUTE,    XXXXXXX, KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_BSLS,
-                     KC_LALT, KC_LSFT, NAV,  KC_SPC, KC_BSPC,    KC_ENT,  KC_LSFT, NAV,     SYM,     KC_LWIN),
+                     KC_LALT, KC_LSFT, SYM,  KC_SPC, KC_BSPC,    KC_ENT,  KC_LSFT, SYM,     NAV,     KC_LWIN),
 
 /* SYM
  * ,----------------------------------------.                         ,-----------------------------------------.
